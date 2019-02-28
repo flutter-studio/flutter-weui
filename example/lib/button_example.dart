afc264ad19bb9173e0cd034fdfd9da684226ff1d
黎明 <2534290808@qq.com>
2019-02-28 09:45:27 +0800

    feat(add button example):

---
+++ example/lib/button_example.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weui/flutter_weui.dart';
+
+class ButtonExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Button"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(5),
+        child: ListView(
+          children: <Widget>[
+            Button(),
+            Button(type: ButtonType.btnWarn),
+            Button(type: ButtonType.btnPrimary),
+            Button(loading: true),
+            Button(disabled: true),
+            Button(text: "按钮"),
+          ],
+        ),
+      ),
+    );
+  }
+}
