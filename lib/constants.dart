68322eee3d298394ca40ab5e016553d7fda136e0
黎明 <2534290808@qq.com>
2019-04-01 17:36:49 +0800

    Create constants.dart

---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class Constants {
+  static Color defaultColor = Color(0xFFF8F8F8);
+  static Color  primaryColor = Color(0xFF1AAD19);
+  static Color warnColor = Color(0xFFE64340);
+  static Color borderColor = Color(0xFFE5E5E5);
+
+
+  static double paddingVertical = 10;
+  static double paddingHorizontal = 15;
+  static double btnFontSize = 18;
+  static double btnHeight = 46;
+}
